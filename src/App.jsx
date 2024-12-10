05fea027a17fad95617aa0592ef23fb725496073
Muffakir Hamid <muffakir63@gmail.com>
2024-12-10 23:14:20 +0530

    Full project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'; // Importing the CSS for styles
-import Navbar from './components/Navbar';
-import Button from './components/Button';
-import MemeContainer from './components/MemeContainer';
 import { ToastContainer } from 'react-toastify';
 import MainScreen from './components/MainScreen';
 import 'react-toastify/dist/ReactToastify.css';
